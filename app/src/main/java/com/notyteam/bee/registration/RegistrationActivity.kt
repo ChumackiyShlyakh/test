5d92a7b6d84f34f06e9f2f99875a5b618f322965
Sasha <samamova@gmail.com>
2019-11-04 17:36:58 +0200

    fix

--- app/src/main/java/com/notyteam/bee/registration/RegistrationActivity.kt
+++ app/src/main/java/com/notyteam/bee/registration/RegistrationActivity.kt
@@ -3,32 +3,27 @@ package com.notyteam.bee.registration
 import android.os.Bundle
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD
 import com.notyteam.bee.utils.AppBarCustom
-=======
 import com.notyteam.bee.R
 import com.notyteam.bee.utils.OnBackPressed
->>>>>>> test-master
+
 
 class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.notyteam.bee.R.layout.activity_registration)
+        setContentView(R.layout.activity_registration)
 
-<<<<<<< HEAD
         AppBarCustom.makeStatusBarTransparent(RegistrationActivity@this)
 
         supportFragmentManager.beginTransaction().replace(com.notyteam.bee.R.id.fragment_container_registration,
             RegistrationFragment()).commit()
-=======
         window.statusBarColor = getResources().getColor(R.color.orange_light)
         supportFragmentManager.beginTransaction().replace(
             R.id.fragment_container_registration,
             RegistrationFragment()
         ).commit()
     }
->>>>>>> test-master
 
     override fun onBackPressed() {
         val fragment =
