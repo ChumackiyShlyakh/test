314e1f3a9b2c819ac5383e91fb1a8eb4629894a9
Sashka <klondak10@gmail.com>
2019-11-06 10:23:41 +0200

    fix

--- app/src/main/java/com/notyteam/bee/topics/my_places/MyPlacesFragment.kt
+++ app/src/main/java/com/notyteam/bee/topics/my_places/MyPlacesFragment.kt
@@ -1,24 +1,33 @@
 package com.notyteam.bee.topics.my_places
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.notyteam.bee.R
+import com.notyteam.bee.core.ui.MainActivity
+import com.notyteam.bee.topics.my_places.controls.MyPlacesControlsBeehousesFragment
 import com.notyteam.bee.topics.my_places.adapter.FragmentWithViewPager
 import com.notyteam.bee.topics.my_places.adapter.MyFragmentPagerAdapter
 import com.notyteam.bee.topics.my_places.adapter.PagerAdapter
 import com.notyteam.bee.topics.my_places.viewpager_fragments.*
 
-class MyPlacesFragment : Fragment() {
+class MyPlacesFragment : Fragment(), View.OnClickListener {
 
     private lateinit var viewpager: ViewPager
     private lateinit var tabs: TabLayout
+
+    var imgbtn_fragment_myplaces: ImageButton? = null
+    var adapter: MyFragmentPagerAdapter? = null
+
     private lateinit var pagerAdapter: PagerAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +36,46 @@ class MyPlacesFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_myplaces, container, false)
 
+
+        tabs = view.findViewById(R.id.tabs_fragment_myplaces)
+        viewpager = view.findViewById(R.id.viewpager)
+        adapter = MyFragmentPagerAdapter(childFragmentManager)
+//        imgbtn_fragment_myplaces = view.findViewById(R.id.imgbtn_fragment_myplaces)
+//        imgbtn_fragment_myplaces?.setOnClickListener({
+//                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 1: ")
+//        })
+
+        MainActivity.imgbtn_controls_my_places_beehouses?.setOnClickListener({
+
+            when (viewpager.currentItem) {
+                0 -> {
+                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 1: ")
+                }
+                1 -> {
+                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 2: ")
+                }
+                2 -> {
+                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 3: ")
+                }
+                3 -> {
+                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 4: ")
+                }
+                4 -> {
+                    Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 5: ")
+                }
+            }
+
+
+
+
+//            if (adapter?.getItemId(1) == 1L){
+//                Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 1: ")
+//            }
+//            else{
+//                Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 3: ")
+//            }
+            Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 5: " + adapter?.getItem(1) + " ItemId " + adapter?.getItemId(id))
+        })
         val fragments = arrayOf(
             instantiate(context!!, VPBeehousesFragment::class.java.getName()),
             instantiate(context!!, VPApiaryFragment::class.java.getName()),
@@ -35,13 +84,25 @@ class MyPlacesFragment : Fragment() {
             instantiate(context!!, VPArchiveFragment::class.java.getName())
         )
 
-        tabs = view.findViewById(R.id.tabs)
-        viewpager = view.findViewById(R.id.viewpager)
+
         pagerAdapter = PagerAdapter(childFragmentManager, fragments)
 
         setupViewPager()
 
         return view
+    }
+
+    override fun onClick(view: View?) {
+//        when (adapter?.count) {
+            Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 2: ")
+//            1 -> {
+//                Log.d("imgbtn_fragment", "imgbtn_fragment_myplaces 2: ")
+//                childFragmentManager.beginTransaction().replace(
+//                    R.id.fragment_container_main_activity,
+//                    MyPlacesControlsBeehousesFragment()
+//                ).commit()
+
+//            }
     }
 
     private fun setupViewPager() {
@@ -64,6 +125,7 @@ class MyPlacesFragment : Fragment() {
 
 //        viewpager.offscreenPageLimit
         viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
 
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
 
